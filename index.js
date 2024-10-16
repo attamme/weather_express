3d7c0001f65d33f6af6ed4a360a735623f964699
Annaliisa Tamme <annaliisa.tamme@voco.ee>
2024-10-16 09:49:24 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -6,9 +6,32 @@ const fetch = require('node-fetch');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//proceed form post method data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const key = 'a8d3e9c881cdf768ab1d009883be1b38';
 let city = 'Tartu';
+
 app.get('/', function (req, res) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((responce) => {
+        return responce.json()
+    })
+    .then((data) => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)- 273.15)
+        res.render('index', {
+            description: description, 
+            city: city, 
+            temp: temp
+        })
+    })
+})
+
+app.post('/', function(req, res){
+    let city = req.body.cityname
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
     .then((responce) => {
         return responce.json()
